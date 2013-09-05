b22e8ac4f210ad4f8baa01751ccece15dd134f0e
tigerandy163com <tigerandy@163.com>
2013-09-05 14:07:46 +0800

    1

---
+++ AA/LoadLevelInfo.cpp
@@ -0,0 +1,91 @@
+//
+//  LoadLevelInfo.cpp
+//  AA
+//
+//  Created by chunyu.wang on 13-9-5.
+//
+//
+
+#include "LoadLevelInfo.h"
+
+//
+//
+#include "LoadLevelinfo.h"
+LoadLevelinfo::LoadLevelinfo()
+{
+}
+LoadLevelinfo::~LoadLevelinfo()
+{
+    
+    //TODO： 安全删除s_arr
+    //f_ClearAll();
+    // CC_SAFE_RELEASE(s_LevelParameter);
+    // CC_SAFE_RELEASE(s_NeedToLoadImages);
+}
+LoadLevelinfo *LoadLevelinfo::createLoadLevelinfo(const char *plistpath)
+{
+    LoadLevelinfo *pRet = new LoadLevelinfo();
+    if (pRet && pRet->f_SetPlist(plistpath))
+    {
+        pRet->autorelease();
+        return pRet;
+    }
+    else
+    {
+        delete pRet;
+        pRet = NULL;
+        return NULL;
+    }
+}
+//变更plist
+bool LoadLevelinfo::f_SetPlist(const char *plistpath)
+{
+    
+    bool bRet = false;
+    do
+    {
+        /* code */
+        // if (s_LevelParameter != NULL && s_NeedToLoadImages != NULL)
+        // f_ClearAll();
+        //创建一个实力
+        CCDictionary *ccd = CCDictionary::createWithContentsOfFile(plistpath);
+        CC_BREAK_IF(!ccd);
+        //进入关卡所需加载图片资源的节点
+        s_NeedToLoadImages = dynamic_cast<CCDictionary *>(ccd->objectForKey(NEEDTOLOADIMAGES));
+        CC_BREAK_IF(!s_NeedToLoadImages);
+        //进入关卡所需信息节点
+        s_LevelParameter = dynamic_cast<CCDictionary *>(ccd->objectForKey(LEVELPARAMETER));
+        CC_BREAK_IF(!s_LevelParameter);
+        //You need to remove : 输出调试信息
+        CCLog("Needtoloadimages count is : %d", s_NeedToLoadImages->count());
+        CCLog("levelparameter count is : %d", s_LevelParameter->count());
+        bRet = true;
+    }
+    while (0/* condition */);
+    return bRet;
+}
+//清空已经读取的字符串
+void LoadLevelinfo::f_ClearAll()
+{
+    s_NeedToLoadImages->removeAllObjects();
+    s_LevelParameter->removeAllObjects();
+}
+//获取关卡信息 根据key
+float LoadLevelinfo::f_GetLevelInfo(const char *key)
+{
+    CCString * temp =   dynamic_cast<CCString*>(s_LevelParameter->objectForKey(key));
+    return temp->floatValue();
+}
+const char * LoadLevelinfo::f_GetLevelTmxPath(const char * key)
+{
+    CCString * temp =   dynamic_cast<CCString*>(s_LevelParameter->objectForKey(key));
+    return temp->getCString();
+    
+}
+//获取预加载图片的容器
+const char  *LoadLevelinfo::f_GetLoadingImages(int key)
+{
+    CCString * pString  = CCString ::createWithFormat("%d",key);
+    CCString * temp =   dynamic_cast<CCString*>(s_NeedToLoadImages->objectForKey(pString->getCString()));
+    return temp->getCString();
+}
---
+++ AA/LoadLevelInfo.h
@@ -0,0 +1,58 @@
+//
+//  LoadLevelInfo.h
+//  AA
+//
+//  Created by chunyu.wang on 13-9-5.
+//
+//
+
+#ifndef __AA__LoadLevelInfo__
+#define __AA__LoadLevelInfo__
+
+#include "cocos2d.h"
+using namespace cocos2d;
+#define NEEDTOLOADIMAGES "needtoloadimages"
+#define LEVELPARAMETER "levelparameter"
+class LoadLevelinfo: public CCObject
+{
+public:
+    //构造函数
+    LoadLevelinfo();
+    ~LoadLevelinfo();
+    static LoadLevelinfo * createLoadLevelinfo(const char *plistpath);
+    //变更plist
+    bool f_SetPlist(const char * plistpath);
+    
+    //获取关卡信息 根据key
+    float f_GetLevelInfo(const char * key);
+    //获得tmx瓦片地图文件路径
+    const char * f_GetLevelTmxPath(const char * key);
+    /*
+     获取预加载图片的容器
+     读取的plist格式是这个样子滴
+     <key>4</key>
+     <string>loadingHMenu/111.plist</string>
+     <key>3</key>
+     <string>levelselectone/themescene2-hd.plist</string>
+     <key>2</key>
+     <string>levelselectone/stages_bg-hd.plist</string>
+     <key>1</key>
+     <string>Hmenu/mainscene1-hd.plist</string>
+     */
+    const char  * f_GetLoadingImages(int key);
+    
+    
+    //清空已经读取的字符串
+    void f_ClearAll();
+    /* data */
+private:
+    //保存关卡需要预加载的图片
+    CCDictionary * s_NeedToLoadImages;
+    
+    //保存关卡的相关的数值信息
+    CCDictionary * s_LevelParameter;
+    
+private:
+    
+};
+#endif /* defined(__AA__LoadLevelInfo__) */
