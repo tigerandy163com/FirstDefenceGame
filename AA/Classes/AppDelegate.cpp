2803022532b392f49cb3111e7ef296a99e7fdaa6
tigerandy163com <tigerandy@163.com>
2013-10-22 18:07:37 +0800

    1

--- AA/Classes/AppDelegate.cpp
+++ AA/Classes/AppDelegate.cpp
@@ -25,16 +25,17 @@ AppDelegate::~AppDelegate()
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
+   
     // initialize director
     CCDirector *pDirector = CCDirector::sharedDirector();
     pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
-
+     CCEGLView::sharedOpenGLView()->setDesignResolutionSize(480, 320,kResolutionExactFit);
     // turn on display FPS
     pDirector->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
-
+    
     // create a scene. it's an autorelease object
     CCScene *pScene = StartScene::scene();
 
